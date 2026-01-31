96bc4d3464ae7547ee7afc2fd6c38f41f1df4816
vamsi <vamsikrishnapallam@gmail.com>
2026-01-31 10:13:47 +0530

    fix: dynamic IP detection for QR codes to allow mobile verification

--- utils/qr_utils.py
+++ utils/qr_utils.py
@@ -1,15 +1,23 @@
 import qrcode
 import os
 import config
+import socket
+
+def get_local_ip():
+    try:
+        # This gets the actual local IP of your machine on the WiFi
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
+    except:
+        return "localhost"
 
 def generate_student_qr(student_data):
-    # Data to encode: Roll No, Name, and a verification placeholder
-    qr_payload = (
-        f"CertID: {student_data['roll_no']}\n"
-        f"Student: {student_data['name']}\n"
-        f"Workshop: {config.WORKSHOP_TOPIC}\n"
-        f"Verified: http://kiet.edu/verify/{student_data['roll_no']}"
-    )
+    local_ip = get_local_ip()
+    # The URL must point to the web_pages folder relative to the server root
+    verify_url = f"http://{local_ip}:8000/generated_certificates/web_pages/{student_data['roll_no']}.html"
     
     qr = qrcode.QRCode(
         version=1,
@@ -17,7 +25,7 @@ def generate_student_qr(student_data):
         box_size=10,
         border=4,
     )
-    qr.add_data(qr_payload)
+    qr.add_data(verify_url)
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
